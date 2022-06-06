19f532152ec2cf73a0a7990d720ce2ac2d7d2711
RAYHunts <151200078@student.upnyk.ac.id>
2022-06-07 00:42:06 +0700

    hello

--- index.js
+++ index.js
@@ -4,31 +4,33 @@ const client = new Client();
 const port = process.env.PORT || 8888;
 const socket = require("socket.io");
 const http = require("http");
-
 const app = express();
 const server = http.createServer(app);
 const io = socket(server);
+const qrcode = require("qrcode");
 
 app.get("/", (req, res) => {
-  req.status = 200;
-  res.send("Hello World!");
-});
-
-client.on("qr", (qr) => {
-  qrcode.generate(qr, { small: true });
-});
-
-client.on("ready", () => {
-  console.log("Client is ready!");
+  res.sendFile(__dirname + "/index.html");
 });
 
 client.initialize();
 
 // io is the socket connection
+io.on("connection", (socket) => {
+  socket.emit("message", "Connecttion established");
+  client.on("qr", (qr) => {
+    qrcode.toDataURL(qr, (err, url) => {
+      socket.emit("qr", url);
+      socket.emit("message", "QR code generated");
+    });
+  });
+  client.on("ready", () => {
+    socket.emit("message", "Client ready");
+  });
+});
 
 server.listen(port, () => {
-  console.log(`Server is running on port ${port} \n
-                http://localhost:${port}`);
+  console.log(`Server is running on port ${port}\nhttp://localhost:${port}`);
 });
 
 client.on("message", (message) => {
