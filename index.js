e311aad7759e58b7e313f2341e54c42da24b6eb2
RAYHunts <151200078@student.upnyk.ac.id>
2022-06-07 01:11:18 +0700

    fix port

--- index.js
+++ index.js
@@ -11,17 +11,17 @@ const client = new Client({
   puppeteer: {
     headless: true,
     args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--disable-accelerated-2d-canvas',
-      '--no-first-run',
-      '--no-zygote',
-      '--single-process', // <- this one doesn't works in Windows
-      '--disable-gpu'
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-accelerated-2d-canvas",
+      "--no-first-run",
+      "--no-zygote",
+      "--single-process", // <- this one doesn't works in Windows
+      "--disable-gpu",
     ],
   },
-}
+});
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
